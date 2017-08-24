313921cbb68948c4045b52bc6c7820e30aca91ae
casperlamboo <casperlamboo@gmail.com>
2017-08-24 13:47:10 +0200

    update text class

--- src/object/text.js
+++ src/object/text.js
@@ -6,55 +6,90 @@ export default class Text extends Matrix {
 		super(options);
 
 		let {
-			visible = true, 
-			active = true, 
-			depth = 0, 
-			text = '', 
-			style = 'normal', 
-			variant = 'normal', 
-			weight = 'normal', 
-			size = 12, 
-			font = 'Arial', 
-			align = 'left', 
-			baseline = 'bottom', 
-			textAlign = 'left', 
-			color = new Color(), 
-			alpha = 'number'
+			visible = true,
+			active = true,
+			depth = 0,
+			text = '',
+			style = 'normal',
+			variant = 'normal',
+			weight = 'normal',
+			size = 12,
+			family = 'Arial',
+			align = 'start',
+			baseline = 'alphabetic',
+			fill = true,
+			fillColor = new Color(),
+			alpha = 1,
+			stroke = false,
+			strokeColor = new Color(),
+			strokeWidth = 1,
+			shadow = false,
+			shadowColor = new Color(),
+			shadowBlur = 0,
+			shadowOffsetX = 0,
+			shadowOffsetY = 0
 		} = options;
 
 		this.visible = visible;
 		this.active = active;
 		this.depth = depth;
+
 		this.text = text;
 		this.style = style;
 		this.variant = variant;
 		this.weight = weight;
 		this.size = size;
-		this.font = font;
+		this.family = family;
 		this.align = align;
 		this.baseline = baseline;
-
-		this.textAlign = textAlign;
-
-		this.color = color;
+		this.fill = fill;
+		this.fillColor = fillColor;
 		this.alpha = alpha;
+		this.stroke = stroke;
+		this.strokeColor = strokeColor;
+		this.strokeWidth = strokeWidth;
+		this.shadow = shadow;
+		this.shadowColor = shadowColor;
+		this.shadowBlur = shadowBlur;
+		this.shadowOffsetX = shadowOffsetX;
+		this.shadowOffsetY = shadowOffsetY;
 	}
 
-	measure (text = this.text) {
-		context.font = `${this.style} ${this.variant} ${this.weight} ${this.size}px ${this.font}`;
+	getStyle() {
+		return `${this.style} ${this.variant} ${this.weight} ${this.size}px ${this.family}`;
+	}
+
+	measure (context, text = this.text) {
+		context.font = this.getStyle();
 
 		return context.measureText(text).width;
 	}
 
 	drawText (context, text, x, y) {
-		context.font = `${this.style} ${this.variant} ${this.weight} ${this.size}px ${this.font}`;
+		context.font = this.getStyle();
 
 		context.textAlign = this.align;
 		context.textBaseline = this.baseline;
-		
-		this.color.setContext(context);
-		
-		context.fillText(text, x, y);
+
+		if (this.stroke) {
+			this.strokeColor.setStroke(context);
+			context.lineWidth = this.strokeWidth;
+			context.strokeColor(text, x, y);
+		}
+		if (this.fill) {
+			if (this.shadow) {
+				this.shadowColor.setShadow(context);
+				context.shadowBlur = this.shadowBlur;
+				context.shadowOffsetX = this.shadowOffsetX;
+				context.shadowOffsetY = this.shadowOffsetY;
+			} else {
+				context.shadowBlur = 0;
+				context.shadowOffsetX = 0;
+				context.shadowOffsetY = 0;
+			}
+			this.fillColor.setFill(context);
+			context.fillText(text, x, y);
+		}
 	}
 
 	drawTextAlpha (context, text, x, y, apha) {
@@ -72,18 +107,34 @@ export default class Text extends Matrix {
 		matrix.setMatrixContext(context);
 
 		this.drawText(context, this.text, 0, 0);
-		
+
 		context.restore();
 	}
 
 	clone () {
 		return new Text({
-			style, 
-			variant, 
-			weight, 
-			size, 
-			font, 
-			color: this.color.clone()
+			visible: this.visible,
+			active: this.active,
+			depth: this.depth,
+			text: this.text,
+			style: this.style,
+			variant: this.variant,
+			weight: this.weight,
+			size: this.size,
+			family: this.family,
+			align: this.align,
+			baseline: this.baseline,
+			fill: this.fill,
+			fillColor: this.fillColor.clone(),
+			alpha: this.alpha,
+			stroke: this.stroke,
+			strokeColor: this.strokeColor.clone(),
+			strokeWidth: this.strokeWidth,
+			shadow: this.shadow,
+			shadowColor: this.shadowColor.clone(),
+			shadowBlur: this.shadowBlur,
+			shadowOffsetX: this.shadowOffsetX,
+			shadowOffsetY: this.shadowOffsetY
 		});
 	}
 };
